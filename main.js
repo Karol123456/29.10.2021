11ed0082533f0bda44654999f789885e34be1283
KacDzi <kacper.dziebor@tlen.pl>
2021-10-29 11:46:28 +0200

    f
    f

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ var button = document.getElementById('hover');
 var body = document.body;
 const portfolio = document.getElementById('hoverdwa');
 const fontsize = document.getElementById('fontsize')
+var welcome = document.getElementById('welcome')
+
+
 
 button.onmouseover = function() {
 	body.className = 'hovered';
     portfolio.className = 'port2';
-    fontsize.className = 'fontsizehover'
-   
+    fontsize.className = 'fontsizehover';
+   welcome.className = 'welcomewhite';
     
+
+  
 }
 
 button.onmouseout = function() {
 	body.className = '';
+    if(welcome.className === 'welcomewhite' ) welcome.className = 'welcomeblack'
 }
 
 
